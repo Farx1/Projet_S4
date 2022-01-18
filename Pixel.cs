5407f8c6febfd344887045b25f2ea7c7d4edbacf
Farx1 <jules.sayad-barth@edu.devinci.fr>
2022-01-18 19:20:51 +0100

    ok

---
+++ Pixel.cs
@@ -0,0 +1,34 @@
+namespace Projet;
+
+public class Pixel
+{
+    private int _red;
+    private int _green;
+    private int _blue;
+
+
+    public Pixel(int red, int green, int blue)
+    {
+        _red = red;
+        this._green = green;
+        this._blue = blue;
+    }
+
+    public int Red
+    {
+        get => _red;
+        set => _red = value;
+    }
+
+    public int Green
+    {
+        get => _green;
+        set => _green = value;
+    }
+
+    public int Blue
+    {
+        get => _blue;
+        set => _blue = value;
+    }
+}
