f5b5f7b27440c08ced9171898277ba44baf6ffcf
Farx1 <jules.sayad-barth@edu.devinci.fr>
2022-01-18 20:27:15 +0100

    okayy

--- Structure.cs
+++ Structure.cs
@@ -2,8 +2,30 @@ namespace Projet;
 
 public static class Structure
 {
-    public static int Convertir_Endian_To_Int(byte[] tab …)
+    public static int Convertir_Endian_To_Int(byte[] tab)
     {
+        int result = 0;
+        for (int i = 0; i < tab.Length; i++)
+        {
+            result = (int) (result + tab[i] * Math.Pow(256, i));
+        }
+        return result;
+    }
+
+    public static byte[] Convertir_Int_To_Endian(int val)
+    {
+        byte[] newone = new byte[4];
+        for (int i = 3; i >=0; i--)
+        {
+            newone[i]= ConvertToByte(val);
+        }
+    }
+
+    public static MyImage ReadBMP(string pov)
+    {
+        byte[] myfile = File.ReadAllBytes(pov);
+        var image = new MyImage();
+
+        byte[] bMP = {myfile(0), myfile(1)};
         
     }
-}
