6a98fa13791a61038d28744386722e8f56ca34de
Farx1 <jules.sayad-barth@edu.devinci.fr>
2022-01-18 17:11:29 +0100

    ok

--- Program.cs
+++ Program.cs
@@ -4,7 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
-using System.Drawing.Common;
 using System.Media;
 using System.ComponentModel;
 
+namespace testBitmap0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bitmap b = new Bitmap(100, 200);
+        }
+    }
+}
