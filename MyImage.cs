5c5492eecb9a82f36479dae2297f8ee64b139587
Farx1 <jules.sayad-barth@edu.devinci.fr>
2022-01-18 18:48:08 +0100

    ok

--- MyImage.cs
+++ MyImage.cs
@@ -1,6 +1,26 @@
-namespace Projet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Media;
+using System.ComponentModel;
+using System.Diagnostics;
 
-public class MyImage
+namespace Projet
 {
-    
+    public class MyImage
+    {
+        private byte[] perroquet = File.ReadAllBytes("Images/coco.bmp");
+
+        public void Propriété()
+        {
+            if (perroquet[0] == 66)
+            {
+
+            }
+        }
+    }   
 }
+
