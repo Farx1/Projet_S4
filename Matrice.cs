76d5423ae67bcc21a5fe7aa0c61515bbe5ce2d7f
Farx1 <jules.sayad-barth@edu.devinci.fr>
2022-02-17 19:08:01 +0100

    oki

--- Matrice.cs
+++ Matrice.cs
@@ -8,38 +8,40 @@ public static class Matrice
         {
             return new int[,]
             {
-                {1,1,1}, {1,1,1}, {1,1,1}
+                {1, 1, 1}, {1, 1, 1}, {1, 1, 1}
             };
         }
     }
-    
+
     public static int[,] Contour
     {
         get
         {
             return new int[,]
             {
-                {0,1,0},{1,-4,1},{0,1,0}
+                {0, 1, 0}, {1, -4, 1}, {0, 1, 0}
             };
         }
     }
+
     public static int[,] Renforcement
     {
         get
         {
             return new int[,]
             {
-                {0,0,0},{-1,1,0},{0,0,0}
+                {0, 0, 0}, {-1, 1, 0}, {0, 0, 0}
             };
         }
     }
+
     public static int[,] Repoussage
     {
         get
         {
             return new int[,]
             {
-                {-2,-1,0},{-1,1,1},{0,1,2}
+                {-2, -1, 0}, {-1, 1, 1}, {0, 1, 2}
             };
         }
     }
@@ -50,19 +52,32 @@ public static class Matrice
         {
             return new int[,]
             {
-                {-1,0,1},{-2,0,2},{-1,0,1}
+                {-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}
             };
         }
     }
+
     public static int[,] Sobel2
     {
         get
         {
             return new int[,]
             {
-                {-1,-2,-1},{0,0,0},{1,2,1}
+                {-1, -2, -1}, {0, 0, 0}, {1, 2, 1}
             };
         }
     }
-    
+
+    public static int[,] Null
+    {
+        get
+        {
+            return new int[,]
+            {
+                {0, 0, 0}, {0, 0, 0}, {0, 0, 0}
+            };
+        }
+
+    }
+
 }
